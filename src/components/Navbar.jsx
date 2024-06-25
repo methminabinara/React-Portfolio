fa2cb49847799c917a54273f7aad2cc2e70b9eac
Methmina Binara <binaraepa@gmail.com>
2024-06-25 22:24:48 +0530

    feat: Added logo to navbar

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,13 @@
+import logo from "../assets/kevinRushLogo.png";
+
+const Navbar = () => {
+  return (
+    <nav className="bg-red-900 mb-20 flex items-center justify-between py-6">
+      <div className="flex flex-shrink-0 items-center">
+        <img src={logo} alt="logo" />
+      </div>
+    </nav>
+  );
+};
+
+export default Navbar;
