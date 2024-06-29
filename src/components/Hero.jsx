e1fd1934980e81eb4c01278fd5b0acfb1a542b50
Methmina Binara <binaraepa@gmail.com>
2024-06-29 18:50:12 +0530

    WIP

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -1,5 +1,6 @@
 import { HERO_CONTENT } from "../constants";
 import profilePic from "../assets/kevinRushProfile.png";
+import { motion } from "framer-motion";
 
 const Hero = () => {
   return (
@@ -7,9 +8,12 @@ const Hero = () => {
       <div className="flex flex-wrap">
         <div className="w-full lg:w-1/2">
           <div className="flex flex-col items-center lg:items-start">
-            <h1 className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl">
+            <motion.h1
+              initial={{}}
+              className="pb-16 text-6xl font-thin tracking-tight lg:mt-16 lg:text-8xl"
+            >
               Kevin Rush
-            </h1>
+            </motion.h1>
             <span className="bg-gradient-to-r from-pink-300 via-slate-500 to-purple-500 bg-clip-text text-3xl tracking-tight text-transparent">
               Full Stack Developer
             </span>
